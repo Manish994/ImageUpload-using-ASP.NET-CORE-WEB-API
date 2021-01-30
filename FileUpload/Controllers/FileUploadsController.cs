eb058053701bae96d2c87287c6f704fc047c03b8
Manish Pokhrel <paulsimon227@gmail.com>
2021-01-30 21:19:21 +0545

    Added IWebHostEnvironment from Controller, for IWebHostEnvironment must be defined using Constructor Injection , [Hint--Constructor Injection can be defined from Controller||Startup.cs]

--- FileUpload/Controllers/FileUploadsController.cs
+++ FileUpload/Controllers/FileUploadsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,29 @@ namespace FileUpload.Controllers
     [ApiController]
     public class FileUploadsController : ControllerBase
     {
+        /* Why derive from ControllerBase vs Controller???
+          -> It is not strictily necessaary, just more to the point. The Controller class derives from 
+             ControllerBase and adds some members that are needed to support Views.
+
+          public class Controller : ControllerBase
+           {
+               public dynamic ViewBag {get;}
+               public virtual ViewResult View(object model) {}
+               // more view support stuff
+           }
+        */
+
+        /*    In MVC i used Server.MapPath for accessing basepath and rootpath.
+              Here IWebHostEnvironment is used for accessing basepath(Directory--wwwroot) and contentpath(FileUploaad-- root)
+              IWebHostEnvironment must define inside constructor injection.
+              I can define IWebHostEnvironment inside startup.cs and Controller.
+          */
+        public static IWebHostEnvironment _webHostEnvironment;
+        public FileUploadsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+         
+        }
+         
     }
 }
